1bd19dcf9279fd81836d503623666fde2d8146f6
h4llow3En <development@felixdoering.com>
2020-11-20 07:23:01 +0100

    Remove usage of deprecated funtions in jQuery

--- _js/menu.js
+++ _js/menu.js
@@ -1,13 +1,13 @@
 
-$(document).ready(function () {
-    $(window).scroll(function () {
+$(function () {
+    $(window).on("scroll", function () {
         if ($(window).scrollTop() > 100) {
             $('.logo.front').removeClass('hidden');
         } else {
             $('.logo.front').addClass('hidden');
         }
     });
-    $('.mobile-toggle').click(function () {
+    $('.mobile-toggle').on("click", function () {
         if ($('.navigation').hasClass('open-nav')) {
             $('.navigation').removeClass('open-nav');
         } else {
