b9184399574511dd58d641d146e9cd34b5fb7b59
h4llow3En <development@felixdoering.com>
2020-11-20 08:46:47 +0100

    Start working on teaser

--- _js/menu.js
+++ _js/menu.js
@@ -1,7 +1,7 @@
 
 $(function () {
     $(window).on("scroll", function () {
-        if ($(window).scrollTop() > 100) {
+        if ($(window).scrollTop() > $(window).height()) {
             $('.logo.front').removeClass('hidden');
         } else {
             $('.logo.front').addClass('hidden');
